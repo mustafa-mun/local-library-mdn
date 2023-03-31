0e1c5435e00237d4d75f920162ff355bcd68e9ca
Mustafa <mustafamcm9@gmail.com>
2023-03-31 18:21:48 +0300

    env variable typo fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var limiter = RateLimit({
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI || URI;
+const mongoDB = process.env.MONGODB_URI || process.env.URI;
 
 async function main() {
   await mongoose.connect(mongoDB);
